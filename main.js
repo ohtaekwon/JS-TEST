088ca04625d8c825602e44f773a03361cae3d33a
ohtaekwon <ohtk1119@gmail.com>
2022-02-13 19:42:38 +0900

    'JSON'

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
-// JSON
-// 자바스크립트 객체 표기법
-
-import myData from './myData.json'
-
-// JSON이라는 확장자를 가지는 파일은 하나의 문자 데이터이다.
-console.log(myData) 
-
-
-const user={
-  name:'OTK',
-  age : 30,
-  emails : [
+const user ={
+  name:'otk',
+  age:30,
+  emails:[
     'ohtk92@naver.com',
-    'helloworld@naver.com',
-  ],
-  // -가 쓰고 싶을때 ''
-  'company-name':{}
+    'neo@zillinks.com'
+  ]
 }
-console.log('user',user)
 
-// JSON이라는 객체에서 stringify라는 메서드 사용
-// JSON은 자바스크립트내 전역 객체
-// stringify는 JSON의 포멧으로 문자데이터화 시키는 메서드
-const str = JSON.stringify(user)
-console.log('str',str) // str {"name":"OTK","age":30,"emails":["ohtk92@naver.com","helloworld@naver.com"],"company-name":{}}
-console.log(typeof str) // string
 
-// JSON은 경량화를 시켜야하는 구조이고 문자데이터로 관리
-// 실제 JS로 쓸려면 JSON.parse를 사용
-// parse는 분석
-const obj = JSON.parse(str)
-console.log('obj',obj)
+// 1. localstorage에 setItem()라는 메서드를 통해서 데이터를 저장
+// 1-1. setItem(저장할 데이터의 이름, 가져올 객체 데이터)
+// 2. 자바스크립트 데이터를 전부다 문자 데이터로 변환하여 저장해야 한다.
+localStorage.setItem('user', JSON.stringify(user))
+
+// 3. localstorage에 저장된 데이터를 다시 js에서 가져와서 출력하기 위해서는, getItem
+// 3-1. getItem(데이터의 이름)
+console.log(localStorage.getItem('user')) 
+// 3-2. {"name":"otk","age":30,"emails":["ohtk92@naver.com","neo@zillinks.com"]}
+// 3-3. 문자데이터 형태로 출력이 되었다.
+
+// 4. js에서 활용하기 좋은 객체 데이터로 변환하기 위해서는, parse
+// 4-1 pasre()
+console.log(JSON.parse(localStorage.getItem('user')))
