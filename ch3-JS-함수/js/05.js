3671fc926449e2af49f8c4d527585334299e7188
ohtaekwon <ohtk1119@gmail.com>
2022-02-27 22:23:39 +0900

    타이머함수.js,콜백함수.js

--- ch3-JS-함수/js/05.js
+++ ch3-JS-함수/js/05.js
@@ -1,3 +1,9 @@
+// 타이머 함수
+// setTimeout(함수, 시간) : 일정 시간 후 함수 실행
+// setInterval(함수, 시간) : 시간 간격마다 함수 실행
+// clearTimeout() : 설정된 Timeout 함수를 종료
+// clearInterval() : 설정된 Interval 함수를 종료
+
 // 1.1. setTimeout - 익명함수
 setTimeout(function(){
   console.log('TK')
--- ch3-JS-함수/js/06.js
+++ ch3-JS-함수/js/06.js
@@ -1,41 +1,28 @@
-// 타이머 함수
-// setTimeout(함수, 시간) : 일정 시간 후 함수 실행
-// setInterval(함수, 시간) : 시간 간격마다 함수 실행
-// clearTimeout() : 설정된 Timeout 함수를 종료
-// clearInterval() : 설정된 Interval 함수를 종료
+// 콜백(Callback)
+// 함수의 인수로 사용되는 함수
 
-// 1.1. setTimeout
-setTimeout(function(){
-  console.log('setTimeout-함수선언 : TK')
-},3000)
-// 3초 뒤에 TK 가 나온다.
+// 1. setTimeout(함수, 시간)
+function timeout(){
+  setTimeout(()=>{
+    console.log('TK')
+  },3000)
+}
+timeout()
+console.log('Done!')
 
-// 1.2. setTimeout - 화살표 함수
-setTimeout(()=>{
-  console.log('setTimout-화살표함수 : TK')
-},3000);
-
-// 2.1. clearTimeout
-const timer = setTimeout(() => {
-  console.log('clearTimeout : TK')
-}, 3000);
-
-// 2.1.1.
-// h1 태그를 선택
-const h1El = document.querySelector('h1')
-// 클릭했을시 종료
-h1El.addEventListener('click',()=>{
-  clearTimeout(timer)
+// 2. 콜백 함수
+function timeout2(callback){
+  setTimeout(()=>{
+    console.log('TK')
+    // 새로 받은 인수를 다음으로 작성한다.
+    callback()
+  },3000)
+}
+// timeout2 인수에 새로운 화살표 함수
+// 콜백 함수
+timeout2(()=>{
+  console.log('Done!')
 })
-
-// 3.1. clearInterval
-const timer2 = setInterval(()=>{
-  console.log('clearInterval : TK')
-},3000)
-
-// 3.2.
-const h1El2 = document.querySelector('h1')
-// 클릭했을 시에 콘솔에 출력 x
-h1El2.addEventListener('click',()=>{
-  clearInterval(timer2)
-})
+// TK
+// Done!
+// 차례대로 출력이 된다. 
