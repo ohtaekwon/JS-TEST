bdd907c54429ec603bbb5c167badd4590cfc96f3
ohtaekwon <ohtk1119@gmail.com>
2022-02-27 23:24:09 +0900

    생성자함수

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-// 콜백(Callback)
-// 함수의 인수로 사용되는 함수
-
-// 1. setTimeout(함수, 시간)
-function timeout(){
-  setTimeout(()=>{
-    console.log('TK')
-  },3000)
+const tk = {
+  firstName:'Oh',
+  lastName:'TK',
+  getFullName:function(){
+    return `${this.firstName} ${this.lastName}`
+  }
 }
-timeout()
-console.log('Done!')
-
-// 2. 콜백 함수
-function timeout2(callback){
-  setTimeout(()=>{
-    console.log('TK')
-    // 새로 받은 인수를 다음으로 작성한다.
-    callback()
-  },3000)
-}
-// timeout2 인수에 새로운 화살표 함수
-// 콜백 함수
-timeout2(()=>{
-  console.log('Done!')
-})
-// TK
-// Done!
-// 차례대로 출력이 된다. 
+console.log(tk)
